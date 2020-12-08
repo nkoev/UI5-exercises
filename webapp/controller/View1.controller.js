3c01bf413f698304130d14a65a165962deffea9a
nkoev <koev.niki@gmail.com>
2020-12-08 08:37:38 +0000

    controller update

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -16,8 +16,6 @@ sap.ui.define([
 			};
 			var model = new JSONModel(data);
 			this.getView().setModel(model);
-			var productsModel = this.getView().getModel('productsModel');
-			// this.getView().setModel("productsModel");
 		},
 		onShowHello: function () {
 			MessageToast.show("Hello World");
